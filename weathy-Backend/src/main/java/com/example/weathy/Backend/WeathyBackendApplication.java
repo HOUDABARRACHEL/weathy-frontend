09078c0631109b9a4e2837a8a0053ee6a701989d
barrachel_houda <houdabarrachel@gmail.com>
2024-12-23 12:20:46 +0100

    initial project

---
+++ weathy-Backend/src/main/java/com/example/weathy/Backend/WeathyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.weathy.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeathyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeathyBackendApplication.class, args);
+	}
+
+}
---
+++ weathy-Backend/src/test/java/com/example/weathy/Backend/WeathyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weathy.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeathyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
